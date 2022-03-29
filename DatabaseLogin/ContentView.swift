2ee91db9d0cd8bd9e2d463c304d3c2d5e8912390
Stepan Kravtsov <dfg>
2022-03-29 21:01:34 +0300

    Initial Commit

---
+++ DatabaseLogin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DatabaseLogin
+//
+//  Created by Степан Кравцов on 29.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DatabaseLogin/DatabaseLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatabaseLoginApp.swift
+//  DatabaseLogin
+//
+//  Created by Степан Кравцов on 29.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct DatabaseLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
