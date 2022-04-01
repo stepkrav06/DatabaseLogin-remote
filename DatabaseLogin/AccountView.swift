34a0fbef1535c28fe0d4eb1195571fa08bb34407
Stepan Kravtsov <stepankravtsov0402@gmail.com>
2022-04-01 10:50:05 +0300

    added change password button

--- DatabaseLogin/AccountView.swift
+++ DatabaseLogin/AccountView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct AccountView: View {
     @EnvironmentObject var viewModel: AppViewModel
+    @Namespace var namespace
+    @State var show = false
+    @State private var password: String = ""
+    @State private var password2: String = ""
     var body: some View {
         ScrollView {
             VStack {
@@ -21,8 +25,53 @@ struct AccountView: View {
                 .padding()
                 .frame(maxWidth: .infinity, maxHeight: 50)
                 .background(.orange)
+                if !show{
+                    Button(action: {withAnimation{show.toggle()}}){ //change password
+                    Text("Change password")
+                            
+                            .matchedGeometryEffect(id: "title", in: namespace)
+                }
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity, maxHeight: 50)
+                .background(Color.orange.matchedGeometryEffect(id: "bg", in: namespace))
+                .cornerRadius(8)
                 
-            .cornerRadius(8)
+                } else {
+                    
+                    Button(action: {withAnimation{show.toggle()}}){ //change password
+                    VStack{
+                    Text("Change password")
+                        .frame(alignment: .top)
+                        
+                        .matchedGeometryEffect(id: "title", in: namespace)
+                    SecureField(
+                        "New Password",
+                        text: $password
+                    )
+                    .textInputAutocapitalization(.never)
+                    .disableAutocorrection(true)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    SecureField(
+                        "Confirm new password",
+                        text: $password2
+                    )
+                    .textInputAutocapitalization(.never)
+                    .disableAutocorrection(true)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    }
+                    
+                }
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity, maxHeight: 300)
+                .background(Color.orange.matchedGeometryEffect(id: "bg", in: namespace))
+                .cornerRadius(8)
+                
+                }
+                
             }
         }
         .navigationTitle("Account")
