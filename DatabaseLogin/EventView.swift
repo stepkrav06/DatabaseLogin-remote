c6133b421d9349d006f917143ca9a8d94ab62a57
Stepan Kravtsov <dfg>
2022-04-05 23:23:46 +0300

    normal bg

---
+++ DatabaseLogin/EventView.swift
@@ -0,0 +1,20 @@
+//
+//  EventView.swift
+//  DatabaseLogin
+//
+//  Created by Степан Кравцов on 05.04.2022.
+//
+
+import SwiftUI
+
+struct EventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventView()
+    }
+}
