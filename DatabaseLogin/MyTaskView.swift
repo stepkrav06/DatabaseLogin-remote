99d988b7f4cd62c83659e114784a600a52687ce1
Stepan Kravtsov <dfg>
2022-04-11 00:21:30 +0300

    My tasks view fixed (added database observer instead of fetching data every time)

--- DatabaseLogin/MyTaskView.swift
+++ DatabaseLogin/MyTaskView.swift
@@ -17,25 +17,23 @@ struct MyTaskView: View {
             Text("You have \(tasks.count) tasks assigned")
             List{
                 
-                Section {
+                Section(header: Text("Tasks"), footer: Text("\(tasks.count) tasks")) {
                     ForEach(tasks) { task in
                         Text(task.content)
                             .badge(task.importance)
                     }
-                  } header: {
-                    Text("Tasks")
-                  } footer: {
-                    Text("\(tasks.count) tasks")
                   }
             }
+            .listStyle(.insetGrouped)
+            
         }
         .navigationTitle("My Tasks")
         .onAppear{
             var ref = Database.database().reference(withPath: "users")
             let userRef = ref.child(viewModel.currentLoggedUser!.uid)
             let tasksRef = userRef.child("tasks")
-            tasksRef.observeSingleEvent(of: .value, with: { snapshot in
-                var tasks: [String] = []
+            tasksRef.observe(.value) { snapshot in
+                
                 // 3
                 for child in snapshot.children {
                   // 4
@@ -44,35 +42,38 @@ struct MyTaskView: View {
                      {
                         let taskId = snapshot.value as! String
                         if taskId != "placeholder"{
-                            tasks.append(taskId)
+                            self.tasksId.append(taskId)
                         }
+                        
                         
                   }
                 }
-                self.tasksId = tasks
-                }) { (error) in
+                ref = Database.database().reference(withPath: "tasks")
+                
+                for taskId in tasksId{
+                    let taskRef = ref.child(taskId)
+                    
+                    taskRef.observe(.value) { snapshot in
+                        var tasks: [Task] = []
+                        // 3
+                        
+                          // 4
+                            if
+                              let task = Task(snapshot: snapshot)
+                             {
+                                tasks.append(task)
+                                
+                          }
+                        
+                        self.tasks = tasks
+                        } withCancel: { error in
+                            print(error.localizedDescription)
+                        }
+                }
+                } withCancel: { error in
                     print(error.localizedDescription)
                 }
-            ref = Database.database().reference(withPath: "tasks")
-            for taskId in tasksId{
-                let taskRef = ref.child(taskId)
-                taskRef.observeSingleEvent(of: .value, with: { snapshot in
-                    var tasks: [Task] = []
-                    // 3
-                    
-                      // 4
-                        if
-                          let task = Task(snapshot: snapshot)
-                         {
-                            tasks.append(task)
-                            
-                      }
-                    
-                    self.tasks = tasks
-                    }) { (error) in
-                        print(error.localizedDescription)
-                    }
-            }
+            
         }
     }
 }
